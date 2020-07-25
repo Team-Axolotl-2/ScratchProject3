cb1c252a20c060f8f8c780ad1d46b5f825eee318
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-07-25 09:33:07 -0700

    update

--- client/app.js
+++ client/app.js
@@ -7,6 +7,7 @@ import CompanyProfile from './src/layout/CompanyProfile';
 import Home from './src/layout/Home';
 import Login from './src/layout/Login';
 import Register from './src/layout/Register';
+import Settings from './src/layout/Settings';
 
 
 
@@ -22,6 +23,7 @@ const App = () =>  {
       <Route exact path = "/companyprofile" component = {CompanyProfile} />
       <Route exact path = "/login" component = {Login} />
       <Route exact path = "/register" component = {Register} />
+      <Route exact path = "/settings" component = {Settings} />
     </Router>
   )
 }
--- client/src/layout/Settings.js
+++ client/src/layout/Settings.js
@@ -11,6 +11,4 @@ const Settings = () => {
   )
 }
 
-
-
 export default Settings
