6673ef2a5bbae75a627a69b051a500f7e6fcdc71
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-07-25 09:54:00 -0700

    update

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const app = express();
 
+const path = require('path')
+
 const cors = require('cors');
 const Mongoose = require('mongoose');
 
@@ -35,6 +37,10 @@ app.use(
     credentials: true,
   })
 );
+
+
+
+
 
 // Define Routes
 
