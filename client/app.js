aa162b10e8a0e5f23745ebe043926dc7eb5f0d94
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-07-24 19:54:08 -0700

    update

--- client/app.js
+++ client/app.js
@@ -2,6 +2,14 @@ import React, { Fragment } from 'react';
 const ReactDOM = require('react-dom');
 
 
+// importing different pages for routing
+import companyProfile from './layout/companyProfile';
+import home from './layout/home';
+import login from './layout/login';
+import register from './layout/register';
+
+
+
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 
@@ -9,6 +17,12 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 const App = () =>  {
   return (
+    <Router>
+      <Route exact path = "/" component = {home} />
+      <Route exact path = "/companyProfile" component = {companyProfile} />
+      <Route exact path = "/login" component = {login} />
+      <Route exact path = "/register" component = {register} />
+    </Router>
     
   )
 }
