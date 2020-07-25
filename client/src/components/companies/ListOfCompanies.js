2f8c3a5877d7e49e0150d814714e0c08fc14928d
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-07-25 09:37:06 -0700

    update

--- client/src/components/companies/ListOfCompanies.js
+++ client/src/components/companies/ListOfCompanies.js
@@ -1,16 +1,19 @@
 // Create the list of companies
 
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 
 // Create a loop, that will create many company cards
-const ListOfCompanies = () => {
-  return (
-    <div>
-      List of Companies
-    </div>
-  )
+class ListOfCompanies extends Component {
+  render(){
+    return(
+      <div>
+        List of Companies
+      </div>
+    )
+  } 
 }
+
 
 export default ListOfCompanies
