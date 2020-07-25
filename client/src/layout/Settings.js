2b2ce172bd601f292bc9642e53726add6c08c67d
Anthony Lin <anthonylin@MacBook-Pro.local>
2020-07-25 09:30:39 -0700

    update

---
+++ client/src/layout/Settings.js
@@ -0,0 +1,16 @@
+// Register Page
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const Settings = () => {
+  return (
+    <div>
+      Settings Page
+    </div>
+  )
+}
+
+
+
+export default Settings
