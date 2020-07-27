4fb80fec60cb2945e97607cef993c498afa6b5d5
Nicolas Pita <nicolaspita@NMBP.home>
2020-07-27 16:59:44 -0700

    price graph populate data

--- client/src/components/priceGraph.js
+++ client/src/components/priceGraph.js
@@ -4,50 +4,63 @@ import { Line } from 'react-chartjs-2';
 import axios from 'axios'
 
 class PriceGraph extends Component {
+  // used to let react know components won't unmount
+  _isMounted = false;
+
   constructor(props) {
     super(props);
     this.state = {
       name: this.props.name,
       chartData: {
-        labels: [
-          '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 
-          '11', '12', '13', '14', '15', '16', '17', '18', '19',
-          '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30'
-        ],
-        datasets: [
-          {
-            label: 'Book Price',
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            data: [120, 135, 140, 145]
-          }
-        ]
+        labels: [],
+        dataSets: {
+            label: '',
+            backgroundColor: '',
+            data: []
+        }
       }
     }
   }
     // for adding the graph when the api route is set up
   componentDidMount() {
+    this._isMounted = true;
+    const dataArr = [];
+    const editLabelsArr = []
     axios.get('/company/chart-data?interval=daily&symbol=' + this.state.name)
       .then(response => {
-        // console.log('response.data in price graph', response.data);
-        console.log(Object.values(response.data['Time Series (Daily)']));
-        // return response.json()
+        const rawLabelsArr = Object.keys(response.data['Time Series (Daily)']);
+        const respArr = Object.values(response.data['Time Series (Daily)']);
+        // mold the data to make it easier to setState
+        for (let i = 0; i < 30; i += 1) {
+          const currentClose = respArr[i];
+          const currentDay = rawLabelsArr[i];
+          dataArr.push(currentClose['4. close']);
+          editLabelsArr.unshift(currentDay);
+        }
+        // reassign values to use in setState
+        const dataObj = {
+          labels: editLabelsArr,
+          datasets: [{
+              label: ['Closing Price'],
+              backgroundColor: 'rgba(255, 99, 132, 0.2)',
+              data: dataArr,
+          }],
+        }
+        this.setState({ 
+          chartData: dataObj
+        })
       })
       .catch(err=>console.log(err));
   }
 
-  // get chart data from the response object
-  // getChartData() {
-  //   const data = this.state.data;
-  //   if (data.datasets) {
-
-  //   }
-  //   return data
-  // }
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
 
   render() {
     return (
       <div>
-        Historical Price Trend (Monthly)
+        Historical Price Trend (Daily)
           <Line
           options={{
             responsive: true
--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-proposal-class-properties'],
           },
         },
       },
